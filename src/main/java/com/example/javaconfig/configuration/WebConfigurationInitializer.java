c7b3ea686e7c63e84fc4f1d1614bd4d443089ef9
iwntutovibrate <iwntutovibrate@gmail.com>
2018-09-06 14:33:11 +0900

    WebConfigurationInitializer.class - add for UTF-8 encoding with CharacterEncodingFilter

--- src/main/java/com/example/javaconfig/configuration/WebConfigurationInitializer.java
+++ src/main/java/com/example/javaconfig/configuration/WebConfigurationInitializer.java
@@ -1,26 +1,45 @@
 package com.example.javaconfig.configuration;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class WebConfigurationInitializer implements WebApplicationInitializer {
 
 	@Override
-	public void onStartup(ServletContext container) throws ServletException {
+	public void onStartup(ServletContext servletContext) throws ServletException {
 
-		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-		ctx.register(ServletConfiguration.class);
-		ctx.setServletContext(container);
+		// ContextLoaderListener listener = new ContextLoaderListener(rootAppContext);
+		// servletContext.addListener(listener);
 
-		ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
+		AnnotationConfigWebApplicationContext servletConfiguration = new AnnotationConfigWebApplicationContext();
+		servletConfiguration.register(ServletConfiguration.class);
+		servletConfiguration.setServletContext(servletContext);
+
+		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher",
+				new DispatcherServlet(servletConfiguration));
 
 		servlet.setLoadOnStartup(1);
 		servlet.addMapping("/");
+
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		
+		FilterRegistration.Dynamic characterEncoding = servletContext.addFilter("characterEncodingFilter",
+				characterEncodingFilter);
+		
+		characterEncoding.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
 	}
 
 }
