fe997a112561f18fac76af032248693752be0f57
iwntutovibrate <iwntutovibrate@gmail.com>
2018-09-06 16:26:01 +0900

    Add Json View Resolver in ServletConfiguration

--- src/main/java/com/example/javaconfig/configuration/ServletConfiguration.java
+++ src/main/java/com/example/javaconfig/configuration/ServletConfiguration.java
@@ -1,19 +1,29 @@
 package com.example.javaconfig.configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @Configuration
 @EnableWebMvc
@@ -28,19 +38,83 @@ import org.springframework.web.servlet.view.JstlView;
 
 public class ServletConfiguration extends WebMvcConfigurerAdapter{
 
+	@SuppressWarnings("all")
+	private static final Logger logger = LoggerFactory.getLogger(ServletConfiguration.class);
+	
+	
+	/**
+	 * @Function   : View Resource Handlers
+	 * @param      : ResourceHandlerRegistry
+	 * @author     : iwntutovibrate
+	 * @date       : 2018. 09. 06
+	 */
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/res/**").addResourceLocations("/resource/");
 	}
 	
+	
+	
+	/**
+	 * @Function   : Content Negotiating View Resolver
+	 * @param      : ContentNegotiationManager
+	 * @author     : iwntutovibrate
+	 * @date       : 2018. 09. 06.
+	 * @return     : ViewResolver - List<ViewResolver>
+	 */
 	@Bean
-	public ViewResolver viewResolver() {
+	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+		
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setContentNegotiationManager(manager);
+
+		List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
+
+		resolvers.add(jspViewResolver());
+		resolvers.add(jsonViewResolver());
+		return resolver;
+	}
+	
+	
+	
+	/**
+	 * @Function   : JSP View Resolver
+	 * @author     : iwntutovibrate
+	 * @date       : 2018. 09. 06.
+	 * @return     : ViewResolver
+	 */
+	@Bean
+	public ViewResolver jspViewResolver() {
 
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setViewClass(JstlView.class);
 		viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");
-
+		viewResolver.setContentType("text/html");
+		viewResolver.setOrder(2);
+		
 		return viewResolver;
 	}
+	
+	
+	
+	/**
+	 * @Function   : JSON View Resolver
+	 * @author     : iwntutovibrate
+	 * @date       : 2018. 09. 06.
+	 * @return     : ViewResolver
+	 */
+	@Bean
+	public ViewResolver jsonViewResolver() {
+		return new ViewResolver() {
+			
+			@Override
+			public View resolveViewName(String arg0, Locale arg1) throws Exception {
+				MappingJackson2JsonView view = new MappingJackson2JsonView();
+				view.setPrettyPrint(true);
+				return view;
+			}
+		};
+	}
+	
 }
