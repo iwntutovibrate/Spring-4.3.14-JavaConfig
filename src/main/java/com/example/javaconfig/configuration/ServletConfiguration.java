0dd0978d978980cb2b87092f99ff824a6e72ab02
iwntutovibrate <iwntutovibrate@gmail.com>
2018-09-06 14:31:34 +0900

    ServletConfiguration.class - ComponentScan Setting

--- src/main/java/com/example/javaconfig/configuration/ServletConfiguration.java
+++ src/main/java/com/example/javaconfig/configuration/ServletConfiguration.java
@@ -2,17 +2,37 @@ package com.example.javaconfig.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.example.javaconfig")
-public class ServletConfiguration {
+@ComponentScan(basePackages = "com.example.javaconfig",
+			   includeFilters = {
+					@Filter(type = FilterType.ASSIGNABLE_TYPE, classes = Controller.class)
+			   },
+			   excludeFilters = {
+					@Filter(type = FilterType.ASSIGNABLE_TYPE, classes = Service.class),
+					@Filter(type = FilterType.ASSIGNABLE_TYPE, classes = Repository.class)
+			   })
 
+public class ServletConfiguration extends WebMvcConfigurerAdapter{
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/res/**").addResourceLocations("/resource/");
+	}
+	
 	@Bean
 	public ViewResolver viewResolver() {
 
