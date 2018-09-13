3841faf7f83f6bd408d834b4fb380a21611f9842
iwntutovibrate <iwntutovibrate@gmail.com>
2018-09-13 13:40:06 +0900

    Remove ApplicationConfiguration @propertySource

--- src/main/java/com/example/javaconfig/configuration/ApplicationConfiguration.java
+++ src/main/java/com/example/javaconfig/configuration/ApplicationConfiguration.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@PropertySource("classpath:database.properties")
 @ComponentScan(basePackages = "com.example.javaconfig",
 			   excludeFilters = {
 					   @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = Controller.class)
