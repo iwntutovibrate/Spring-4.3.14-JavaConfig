330daafb2b58d19db39fd687022064cdb85f4407
iwntutovibrate <iwntutovibrate@gmail.com>
2018-09-13 11:22:53 +0900

    The project logging change to logback.

--- src/main/java/com/example/javaconfig/main/controller/MainController.java
+++ src/main/java/com/example/javaconfig/main/controller/MainController.java
@@ -1,18 +1,12 @@
 package com.example.javaconfig.main.controller;
 
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.example.javaconfig.main.dto.ExampleDTO;
-import com.example.javaconfig.main.service.ExampleService;
 
 @Controller
 public class MainController {
@@ -29,6 +23,7 @@ public class MainController {
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String mainIndex() {
 		
+		logger.debug("=============== LogBack TEST ===============");
 		return "main/index";
 	}
 
